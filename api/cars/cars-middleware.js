e95224476c006a5206cbc21faee01203f2124924
Kyle Willard <kyleswillard@gmail.com>
2021-05-22 07:59:35 -0700

    MVP

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,9 +1,28 @@
+const Cars = require('./cars-model')
+
 const checkCarId = (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    const car = await Cars.getById(req?.params?.id)
+if(car) {
+  req.car = car
+  next()
+} else {
+  res.status(404).json(`"car with id ${req.params.id} is not found"`)
+}
+  } catch (err) {
+res.status(500).json({message: "Server Error"})
+  }
 }
 
 const checkCarPayload = (req, res, next) => {
   // DO YOUR MAGIC
+   const body = req.body;
+  if (body.name && body.vin) {
+    next();
+  } else {
+    res.status(400).json(message: "<field name> is missing")
+  }
 }
 
 const checkVinNumberValid = (req, res, next) => {
--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,33 @@
 // DO YOUR MAGIC
+const express = require('express')
+const Cars = require('./cars-model')
+
+const router = express.Router()
+
+router.get('/', async (req, res) => {
+  try {
+    const cars = await Cars.getAll()
+    res.json(cars)
+  } catch (err) {
+res.status(500).json({message: "Server Error!"})
+  }
+})
+
+router.get('/:id', async (req, res) => {
+  try {
+const cars = await Cars.getById(req.body)
+res.json(cars)
+  } catch (err) {
+    res.status(500).json({message: "Server Error!"})
+  }
+})
+
+router.post('/', async (req, res) => {
+  try {
+const newCar = await Cars.create(req.body)
+res.status(201).json(newCar)
+  } catch (err) {
+    res.status(500).json({message: "Server Error!"})
+  }
+})
+module.exports = router
